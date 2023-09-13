1832e00c967fe155d229907c6f7ace1f193ce8fb
devin1996 <devinchandula@gmail.com>
2023-09-13 15:12:00 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TimelineParent } from './timelines/timelineParent/timelineParent.component';
+import { TimelineOneComponent } from './timelines/timeline1/timelineOne.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    TimelineOneComponent,
+    TimelineParent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/timelines/timelineParent/timelineParent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'timeline-parent',
+  templateUrl: './timelineParent.component.html',
+  styleUrls: ['./timelineParent.component.css'],
+})
+export class TimelineParent {
+  name = 'Angular';
+}
