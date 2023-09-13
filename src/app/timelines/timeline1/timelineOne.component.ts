fc6db6dbcc79ebc17340485ef34e30d80d619b8e
devin1996 <devinchandula@gmail.com>
2023-09-13 17:39:12 +0530

    3 files modified

--- src/app/timelines/timeline1/timelineOne.component.ts
+++ src/app/timelines/timeline1/timelineOne.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {
+  Timeline,
+  TimelineRow,
+  TimelineModel,
+  TimelineOptions,
+TimelineRowStyle,
+} from 'animation-timeline-js';
 
 @Component({
   selector: 'timeline-one',
@@ -6,5 +13,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./timelineOne.component.css'],
 })
 export class TimelineOneComponent {
-  name = 'Angular';
+  
+  methodTest(){
+    const model = { rows: [] as Array<TimelineRow> } as TimelineModel;
+    const options = {
+    id: "timeline",
+    rowsStyle: {
+      height: 35,
+    } as TimelineRowStyle,
+  } as TimelineOptions;  
+  }
+  
+
+  const timeline = new Timeline(options, model);
 }
