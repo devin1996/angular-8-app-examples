6c2dc0204ad09cb04f46a812272fbfbb12248f10
devin1996 <devinchandula@gmail.com>
2023-09-18 13:03:14 +0530

    Added Several testing options for vis timeline

--- src/app/timelines/timeline1/timelineOne.component.ts
+++ src/app/timelines/timeline1/timelineOne.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  NliTimelineModule,
-  TimeLineModel,
-  TimeLineStepModel,
-} from '@next-level-integration/timeline';
 
 @Component({
   selector: 'timeline-one',
@@ -12,26 +7,5 @@ import {
 })
 export class TimelineOneComponent implements OnInit {
   timelineModel: any;
-  ngOnInit() {
-    const timelineModel = new TimeLineModel(5, [
-      new TimeLineStepModel('17', 'Zählerwechsel17', new Date('2014-04-03')),
-      new TimeLineStepModel('18', 'Zählerwechsel18', new Date('2015-04-03')),
-      new TimeLineStepModel('1', 'Zählerwechsel1', new Date('2016-04-03')),
-      new TimeLineStepModel('2', 'Zählerwechsel2', new Date('2017-04-03')),
-      new TimeLineStepModel('3', 'Zählerwechsel3', new Date('2018-04-03')),
-      new TimeLineStepModel('4', 'Zählerwechsel4', new Date('2019-04-03')),
-      new TimeLineStepModel('5', 'Zählerwechsel5', new Date('2020-04-03')),
-      new TimeLineStepModel('6', 'Zählerwechsel6', new Date('2013-04-03')),
-      new TimeLineStepModel('7', 'Zählerwechsel7', new Date('2012-04-03')),
-      new TimeLineStepModel('8', 'Zählerwechsel8', new Date('2011-04-03')),
-      new TimeLineStepModel('9', 'Zählerwechsel9', new Date('2010-04-03')),
-      new TimeLineStepModel('10', 'Zählerwechsel10', new Date('2009-04-03')),
-      new TimeLineStepModel('11', 'Zählerwechsel11', new Date('2008-04-03')),
-      new TimeLineStepModel('12', 'Zählerwechsel12', new Date('2007-04-03')),
-      new TimeLineStepModel('13', 'Zählerwechsel13', new Date('2006-04-03')),
-      new TimeLineStepModel('14', 'Zählerwechsel14', new Date('2003-04-03')),
-      new TimeLineStepModel('15', 'Zählerwechsel15', new Date('2002-04-03')),
-      new TimeLineStepModel('16', 'Zählerwechsel16', new Date('2000-11-11')),
-    ]);
-  }
+  ngOnInit() {}
 }
--- src/app/timelines/vis-timeline/visTimeline.component.ts
+++ src/app/timelines/vis-timeline/visTimeline.component.ts
@@ -8,11 +8,93 @@ import { DataSet } from 'vis-data';
   templateUrl: './visTimeline.component.html',
   styleUrls: ['./visTimeline.component.css'],
 })
-export class VisTimelineComponent {
+export class VisTimelineComponent implements OnInit {
   timeline: Timeline;
   options: {};
   data: any;
   groups: any;
 
-  //@ViewChild('timeline' ) :;
+  @ViewChild('timeline', { static: true }) timelineContainer: ElementRef;
+
+  constructor() {
+    this.getTimelineData();
+    this.getTimelineGroups();
+    this.getOptions();
+  }
+
+  ngOnInit() {
+    this.timeline = new Timeline(
+      this.timelineContainer.nativeElement,
+      null,
+      this.options
+    );
+    this.timeline.setGroups(this.groups);
+    this.timeline.setItems(this.data);
+  }
+
+  getTimelineGroups() {
+    this.groups = new DataSet([
+      {
+        id: 0,
+        content: 'Group 1',
+      },
+      {
+        id: 1,
+        content: 'Group 2',
+      },
+    ]);
+  }
+
+  getTimelineData() {
+    this.data = new DataSet();
+    this.data.add({
+      id: 1,
+      group: 0,
+      content: 'item 1',
+      start: 0,
+      end: 2,
+      //type: 'box',
+      editable: {
+        remove: true,
+        updateGroup: true,
+        updateTime: true,
+      },
+    });
+  }
+
+  // setting time line options
+  getOptions() {
+    this.options = {
+      stack: true,
+      start: 0,
+      end: 10,
+      // timeAxis: {
+      //   scale: 'hour',
+      //   step: 5,
+      // },
+      //itemsAlwaysDraggable: true,
+      //editable: true,
+      margin: {
+        axis: 0,
+      },
+      showMajorLabels: true,
+      orientation: 'top',
+      //clickToUse: true,
+      editable: {
+        remove: true,
+        add: true,
+      },
+      itemsAlwaysDraggable: {
+        item: true,
+        range: true,
+      },
+      rollingMode: {
+        //follow: true,
+        //offset: '0',
+      },
+      showCurrentTime: true,
+      showTooltips: true,
+      moveable: true,
+    };
+  }
 }
