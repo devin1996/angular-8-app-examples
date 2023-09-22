d7ff0cfe38b63f2be56a97a16bd044cfac6fb712
devin1996 <devinchandula@gmail.com>
2023-09-22 16:19:00 +0530

    Working on Methods

--- src/app/timelines/vis-timeline/visTimeline.component.ts
+++ src/app/timelines/vis-timeline/visTimeline.component.ts
@@ -14,6 +14,15 @@ export class VisTimelineComponent implements OnInit {
   options: {};
   data: any;
   groups: any;
+  date = new Date();
+
+  currentTime: any;
+  customTime: any;
+  eventProperties: any;
+  itemRange: any;
+  selection: any;
+  visibleItems: any;
+  window: any;
 
   @ViewChild('timeline', { static: true }) timelineContainer: ElementRef;
 
@@ -53,8 +62,8 @@ export class VisTimelineComponent implements OnInit {
       id: 1,
       group: 0,
       content: 'CAS1',
-      start: new Date(2023, 8, 21),
-      end: new Date(2023, 8, 22),
+      start: moment().subtract(10, 'm').toISOString(),
+      end: moment().add(10, 'm').toISOString(),
       title: 'Just For Hover',
     });
   }
@@ -78,16 +87,106 @@ export class VisTimelineComponent implements OnInit {
       rollingMode: {
         follow: true,
       },
-      showWeekScale: true,
+      showWeekScale: false,
+      showCurrentTime: true,
       showTooltips: true,
       timeAxis: { scale: 'minute', step: 1 },
-      //zoomMax: 3600000,
-      //zoomMin: 30000, // 30 secs
-      //moment:
+      zoomMax: 900000,
+      zoomMin: 60000, // 1 min
     };
   }
 
-  addCustomTime() {}
+  //Add new vertical bar representing a custom time that can be dragged by the user.
+  addCustomTime(date) {
+    this.timeline.addCustomTime(date);
+  }
+
+  //Destroy the Timeline. The timeline is removed from memory.
+  destroyTimeline() {
+    this.timeline.destroy();
+  }
+
+  fit() {
+    this.timeline.fit();
+  }
+
+  focus() {
+    this.timeline.focus(1);
+  }
+
+  getCurrentTime() {
+    this.currentTime = this.timeline.getCurrentTime();
+  }
+
+  getCustomTime() {
+    this.currentTime = this.timeline.getCustomTime(1);
+  }
+
+  //Didn't completed
+  getEventProperties() {
+    //this.eventProperties = this.timeline.getEventProperties();
+    // group (Number or null): the id of the clicked group.
+    // item (Number or null): the id of the clicked item.
+    // customTime (Number or null): the id of the clicked custom-time.
+    // pageX (Number): absolute horizontal position of the click event.
+    // pageY (Number): absolute vertical position of the click event.
+    // x (Number): relative horizontal position of the click event.
+    // y (Number): relative vertical position of the click event.
+    // time (Date): Date of the clicked event.
+    // snappedTime (Date): Date of the clicked event, snapped to a nice value.
+    // what (String or null): name of the clicked thing: item, background, axis, group-label, custom-time, or current-time.
+    // event (Object): the original click event.
+    // document.getElementById('myTimeline').onclick = function (event) {
+    //   var props = timeline.getEventProperties(event)
+    //   console.log(props);
+    // }
+  }
+
+  getItemRange() {
+    this.itemRange = this.timeline.getItemRange();
+    console.log(this.timeline.getItemRange());
+  }
+
+  getSelection() {
+    this.selection = this.timeline.getSelection();
+  }
+
+  getVisibleItems() {
+    this.visibleItems = this.timeline.getVisibleItems();
+  }
+
+  getWindow() {
+    this.window = this.timeline.getWindow();
+    console.log(this.window);
+  }
+
+  //moveTo(time [, options, callback])
+
+  //Didn't completed
+  clickOn() {
+    this.timeline.on('select', onSelect);
+  }
+
+  onSelect(properties) {
+    alert('selected items: ' + properties.items);
+  }
+
+  on(event, callback) {
+    this.timeline.on('select', function (properties) {
+      alert('selected items: ' + properties.items);
+    });
+
+    function onSelect(properties) {
+      alert('selected items: ' + properties.items);
+    }
+
+    // add event listener
+
+    // do stuff...
+
+    // remove event listener
+    this.timeline.off('select', onSelect);
+  }
 
   zoomIn() {
     this.timeline.zoomIn(0.2);
