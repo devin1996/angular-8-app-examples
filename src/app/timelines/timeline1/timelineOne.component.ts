a201bc992550d392c4cfa703e3318dffc5431b90
devin1996 <devinchandula@gmail.com>
2023-09-25 21:02:43 +0530

    3 files modified

--- src/app/timelines/timeline1/timelineOne.component.ts
+++ src/app/timelines/timeline1/timelineOne.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataSet } from 'vis-data';
+import * as vis from 'vis-timeline';
 
 @Component({
   selector: 'timeline-one',
@@ -6,6 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timelineOne.component.scss'],
 })
 export class TimelineOneComponent implements OnInit {
-  timelineModel: any;
-  ngOnInit() {}
+  ngOnInit(): void {
+    const container = document.getElementById('timeline-container');
+
+    const items = new DataSet([
+      { id: 1, content: 'Event 1', start: 10 },
+      { id: 2, content: 'Event 2', start: 30 },
+    ]);
+
+    const groups = new DataSet([{ id: 1, content: 'Group 1' }]);
+
+    const options = {
+      start: 0,
+      end: 50,
+      format: {
+        minorLabels: {
+          custom: (date, scale, step) => {
+          return date; // Return the numeric value as the label
+          },
+        },
+      },
+    };
+
+    const timeline = new vis.Timeline(container, items, groups, options);
+  }
 }
