5093a8b0b97fc2c7619b1f8f52b7f4f4943daf4e
devin1996 <devinchandula@gmail.com>
2023-10-02 16:50:38 +0530

    Added Clock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TimelineParent } from './timelines/timelineParent/timelineParent.compon
 import { TimelineOneComponent } from './timelines/timeline1/timelineOne.component';
 import { VisTimelineComponent } from './timelines/vis-timeline/visTimeline.component';
 import { AnimationTimelineComponent } from './timelines/animation-timeline/animationTimeline.component';
+import { ClockComponent } from './clock/clock.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
@@ -18,6 +19,7 @@ import { AnimationTimelineComponent } from './timelines/animation-timeline/anima
     TimelineParent,
     VisTimelineComponent,
     AnimationTimelineComponent,
+    ClockComponent,
   ],
   bootstrap: [AppComponent],
 })
---
+++ src/app/clock/clock.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-stopwatch',
+  templateUrl: './clock.component.html',
+  styleUrls: ['./clock.component.css'],
+})
+export class ClockComponent implements OnInit {
+  clock = '';
+  clockHandle;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.clockHandle = setInterval(() => {
+      this.clock = new Date().toLocaleString();
+      console.log('Hi');
+    }, 1000);
+  }
+}
