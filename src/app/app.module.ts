07ff55429977ad26e6c7594a14cfd54802b948b6
devin1996 <devinchandula@gmail.com>
2023-10-16 17:55:27 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,26 +10,13 @@ import { VisTimelineComponent } from './timelines/vis-timeline/visTimeline.compo
 import { AnimationTimelineComponent } from './timelines/animation-timeline/animationTimeline.component';
 import { ClockComponent } from './clock/clock.component';
 import { DialogComponent } from './dialog/dialog-component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatInputModule } from '@angular/material/input';
-import { MatChipsModule } from '@angular/material/chips';
 import { ngxDataTableComponent } from './data-table/ngx-data-table/ngxDataTable.component';
 import { TableFilterPipe } from './data-table/ngx-data-table/table-filter.pipe';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    NgxDatatableModule,
-    NgbModule,
-    // MatFormFieldModule,
-    // MatDialogModule,
-    // MatInputModule,
-    // MatChipsModule,
-  ],
+  imports: [BrowserModule, FormsModule, NgxDatatableModule, NgbModule],
   declarations: [
     AppComponent,
     HelloComponent,
